6bc35d2169bf3979d481285ae155f35b174f7262
heitorlisboa <heitor01101000@gmail.com>
2022-03-14 19:52:28 -0300

    Importação de mais um peso de fonte

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -11,7 +11,7 @@ function Document() {
           crossOrigin="anonymous"
         />
         <link
-          href="https://fonts.googleapis.com/css2?family=Raleway:wght@400;500;700&display=swap"
+          href="https://fonts.googleapis.com/css2?family=Raleway:wght@400;500;600;700&display=swap"
           rel="stylesheet"
         />
       </Head>
