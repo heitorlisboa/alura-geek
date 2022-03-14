9cc95356b152c49bca78505f90b6231917f94939
heitorlisboa <heitor01101000@gmail.com>
2022-03-14 12:59:38 -0300

    Setup inicial do projeto

---
+++ README.md
@@ -0,0 +1,5 @@
+# 3º Alura Challenge de Front-end
+
+Quer saber mais sobre o challenge? Acesse a [página oficial](https://www.alura.com.br/challenges/front-end-3?host=https://cursos.alura.com.br)!
+
+🚧 **PROJETO EM CONSTRUÇÃO** 🚧
---
+++ pages/_app.tsx
@@ -0,0 +1,18 @@
+import Head from "next/head";
+import type { AppProps } from "next/app";
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Head>
+        <meta charSet="UTF-8" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link rel="shortcut icon" href="favicon.png" type="image/png" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
+}
+
+export default MyApp;
