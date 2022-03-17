5d84d8f878acbb9033e6b3e351a85fc3cc795e17
heitorlisboa <heitor01101000@gmail.com>
2022-03-17 12:30:55 -0300

    Refatoração dos estilos
    * Criação de componente de container
    * Alteração do padrão de espaçamento inline

---
+++ src/components/Container/index.tsx
@@ -0,0 +1,18 @@
+import type { FC } from "react";
+
+import styles from "./Container.module.scss";
+
+import { classNames } from "../../utils";
+
+type ContainerProps = {
+  className?: string;
+};
+
+const Container: FC<ContainerProps> = function ContainerComponent({
+  children,
+  className,
+}) {
+  return <div {...classNames([styles.container, className])}>{children}</div>;
+};
+
+export default Container;
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,6 +2,7 @@ import Link from "next/link";
 import { useState } from "react";
 import type { FC } from "react";
 
+import Container from "../Container";
 import Button from "../Button";
 import SearchForm from "../SearchForm";
 import SearchSvg from "../../icons/SearchSvg";
@@ -25,54 +26,52 @@ const Header: FC = function HeaderComponent() {
   }
 
   return (
-    <header className={styles.header}>
-      <div className={styles.titleAndForm}>
-        <h1>
-          <Link href="/" passHref>
-            <a>
-              <span className="sr-only">AluraGeek</span>
-              <img className={styles.logo} src="/svg/logo.svg" alt="Logo" />
-            </a>
-          </Link>
-        </h1>
-
-        {!mobile && <SearchForm />}
-      </div>
-
-      <Button
-        className={styles.loginButton}
-        variant="outlined"
-        as="link"
-        linkHref="/login"
-      >
-        Login
-      </Button>
-
-      {mobile && (
-        <>
-          <button
-            className={styles.toggleSearchButton}
-            type="submit"
-            onClick={openSearchBar}
-          >
-            <span className="sr-only">Abrir barra de pesquisa</span>
-            <SearchSvg className={styles.searchIcon} />
-          </button>
-
-          <div
-            {...classNames([
-              styles.searchFormWrapper,
-              searchBarIsOpen ? styles.active : "",
-            ])}
-          >
-            <SearchForm className={styles.searchForm} />
-            <button onClick={closeSearchBar}>
-              <span className="sr-only">Fechar barra de pesquisa</span>
-              <CloseSvg />
+    <header>
+      <Container className={styles.headerContainer}>
+        <div className={styles.titleAndForm}>
+          <h1>
+            <Link href="/" passHref>
+              <a>
+                <span className="sr-only">AluraGeek</span>
+                <img className={styles.logo} src="/svg/logo.svg" alt="Logo" />
+              </a>
+            </Link>
+          </h1>
+          {!mobile && <SearchForm />}
+        </div>
+        <Button
+          className={styles.loginButton}
+          variant="outlined"
+          as="link"
+          linkHref="/login"
+        >
+          Login
+        </Button>
+        {mobile && (
+          <>
+            <button
+              className={styles.toggleSearchButton}
+              type="submit"
+              onClick={openSearchBar}
+            >
+              <span className="sr-only">Abrir barra de pesquisa</span>
+              <SearchSvg className={styles.searchIcon} />
             </button>
-          </div>
-        </>
-      )}
+            <div
+              {...classNames([
+                styles.searchFormWrapper,
+                searchBarIsOpen ? styles.active : "",
+              ])}
+            >
+              <SearchForm className={styles.searchForm} />
+              <button onClick={closeSearchBar}>
+                <span className="sr-only">Fechar barra de pesquisa</span>
+                <CloseSvg />
+              </button>
+            </div>
+          </>
+        )}
+      </Container>
     </header>
   );
 };
