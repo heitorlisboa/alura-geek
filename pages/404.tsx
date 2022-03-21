6dffebff288cdaacf9d692cb3d9bd32ce24d8982
heitorlisboa <heitor01101000@gmail.com>
2022-03-21 13:39:21 -0300

    Adição de página para erro 404

---
+++ pages/404.tsx
@@ -0,0 +1,27 @@
+import Head from "next/head";
+import type { NextPage } from "next";
+
+import styles from "../src/styles/pages/404.module.scss";
+
+import Container from "../src/components/Container";
+
+const Error404: NextPage = function Error404Page() {
+  return (
+    <>
+      <Head>
+        <title>404: Página não encontrada</title>
+      </Head>
+
+      <main>
+        <Container className={styles.container}>
+          <h2 className={styles.title}>404</h2>
+          <p className={styles.text}>
+            Parece que a página que você está tentando acessar não existe...
+          </p>
+        </Container>
+      </main>
+    </>
+  );
+};
+
+export default Error404;
