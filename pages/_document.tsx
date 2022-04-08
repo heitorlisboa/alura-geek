a3ab3256531aef9502841e7ffb5398ce1e23ecf9
heitorlisboa <heitor01101000@gmail.com>
2022-04-08 17:33:55 -0300

    Adição de meta tags para descrição e imagem

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -14,6 +14,21 @@ function Document() {
           href="https://fonts.googleapis.com/css2?family=Raleway:wght@400;500;600;700&display=swap"
           rel="stylesheet"
         />
+        <meta
+          name="description"
+          content="Projeto de e-commerce desenvolvido durante o 3º Alura Challenge de Front-end"
+        />
+        <meta
+          property="og:image"
+          content="https://alura-geek-heitorlisboa.vercel.app/img/og-image.png"
+        />
+        <meta property="og:image:type" content="image/png" />
+        <meta property="og:image:width" content="800" />
+        <meta property="og:image:height" content="450" />
+        <meta
+          property="og:image:alt"
+          content="AluraGeek. Feito por Heitor Lisboa"
+        />
       </Head>
       <body>
         <Main />
