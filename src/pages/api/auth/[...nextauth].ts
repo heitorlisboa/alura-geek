93038f7bdedcbcacd31e8e66fefb6256c08b21c3
heitorlisboa <heitor01101000@gmail.com>
2023-01-26 02:00:50 -0300

    fix: add type assertion to environment variables

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -4,8 +4,8 @@ import GitHubProvider from "next-auth/providers/github";
 export default NextAuth({
   providers: [
     GitHubProvider({
-      clientId: process.env.GITHUB_ID,
-      clientSecret: process.env.GITHUB_SECRET,
+      clientId: process.env.GITHUB_ID as string,
+      clientSecret: process.env.GITHUB_SECRET as string,
     }),
   ],
 });
