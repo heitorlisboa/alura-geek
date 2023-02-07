1e734cf7bbd3c2e7651b631d9a6284fbfa0341f8
heitorlisboa <heitor01101000@gmail.com>
2023-02-07 13:06:47 -0300

    fix: add missing type and fix unreachable code in `mergeRefs` utility

--- src/utils/mergeRefs.ts
+++ src/utils/mergeRefs.ts
@@ -1,14 +1,16 @@
 import type { AnyMutableRef } from "@/types/misc";
 
 export function mergeRefs<T>(...refs: AnyMutableRef<T>[]) {
-  const filteredRefs = refs.filter(Boolean);
-  if (!filteredRefs) return null;
+  const filteredRefs = refs.filter(Boolean) as NonNullable<
+    typeof refs[number]
+  >[];
+  if (filteredRefs.length === 0) return null;
   if (filteredRefs.length === 1) return filteredRefs[0];
 
   return (instance: T) => {
     for (const ref of filteredRefs) {
       if (typeof ref === "function") ref(instance);
-      else if (ref) ref.current = instance;
+      else ref.current = instance;
     }
   };
 }
